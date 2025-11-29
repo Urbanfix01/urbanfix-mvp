242eef19f61a81b81798d458758c8ba6c3ba3b13
Elias Castillo <info@urbanfixar.com>
2025-11-28 22:20:57 -0300

    Fix: Conectar estilos CSS en Layout

---
+++ client-web/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+// 👇 ¡ESTA ES LA LÍNEA MÁGICA QUE TE FALTA!
+import "./globals.css"; 
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "UrbanFix Web",
+  description: "Portal de presupuestos para clientes",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="es">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
