76f47fe78ced8f040e3cc0a18a79c0fa82530c5e
Elias Castillo <info@urbanfixar.com>
2025-12-02 22:46:35 -0300

    fix: force update next config for pdf

--- client-web/next.config.ts
+++ client-web/next.config.ts
@@ -1,15 +1,15 @@
 import type { NextConfig } from "next";
 
+// Forzando actualización de config
 const nextConfig: NextConfig = {
-  // 1. ESTO ES LO QUE FALTA PARA ARREGLAR EL BUILD
-  serverExternalPackages: ["@react-pdf/renderer"],
+  // Asegúrate de que esta línea esté aquí:
+  serverExternalPackages: ["@react-pdf/renderer"], 
 
   webpack: (config) => {
     config.resolve.alias.canvas = false;
     config.resolve.alias.encoding = false;
     return config;
   },
-
   images: {
     remotePatterns: [
       {
